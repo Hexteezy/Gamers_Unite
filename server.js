8fef70f062da824e1cf8697fb8abc65afa70dbf5
Timothy Olson <tim@Tim-MacBook-Pro.local>
2023-09-20 20:57:59 -0500

    update server.js with grammatical corrections

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const hbs = exphbs.create({});
 
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connections');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
