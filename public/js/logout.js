d160400aa3c43c4b6b604ed58c59cdf9e62e1ebc
Timothy Olson <tim@Tim-MacBook-Pro.local>
2023-09-26 21:19:54 -0500

    BIG UPDATE: set up register route and got login to homepage working, plus some other things

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,11 +1,13 @@
-const logoutHandler = async () => {
-    const respone = await fetch('/api/users/logout', {
+const logoutHandler = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('/api/users/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-        document.location.replace('/login/);')
+        document.location.replace('/login')
     } else {
         alert('Failed to log out!');
     }
