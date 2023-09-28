2c7bbcdd932fead8d546149921d5c6b3c0179304
Timothy Olson <tim@Tim-MacBook-Pro.local>
2023-09-27 22:33:33 -0500

    Profile and partials

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
+const steam = require('../public/js/steamAuth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
@@ -28,4 +29,26 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+router.get('/profile', async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+    const articles = steam
+    console.log(articles)
+
+    res.render('profile', {
+      users,
+      logged_in: req.session.logged_in,
+      articles
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
